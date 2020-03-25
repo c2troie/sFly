13435072b386418f3f48e047c369b5d6fe8b2ff0
Khalid <k_ouazari@hotmail.fr>
2020-03-25 16:02:36 +0100

    Api Express & Mlab connection Db

--- server.js
+++ server.js
@@ -5,7 +5,25 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 const nodemailer = require("nodemailer");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const geoip = require('geoip-lite');
+const MongoClient = require("mongodb").MongoClient;
 
+//CONST ENV
+const MONGO_URL = "mongodb://khalid:khalid123741@ds052629.mlab.com:52629/sfly";
+const COLLECTION_DB = "email";
+const DATA_BASE = "sfly";
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.set('trust proxy', true)
 
 app.use(favicon(__dirname + "/build/favicon.ico"));
 
@@ -20,17 +38,41 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.get("/send-email", function(req, res) {
-  let email = req.query.email
-  console.log("email", email);
-  //console.log('RES',res)
+const insertEmail = async function(email,ip) {
+  try {
+    var geo = geoip.lookup(ip);
+    MongoClient.connect(MONGO_URL, async function(err, client) {
+      if (err) throw err;
+      var db = client.db(DATA_BASE);
+
+      await db
+        .collection(COLLECTION_DB)
+        .insertOne({ 
+          email: email, 
+          timestamp: Date.now(), 
+          adress:{
+            city:geo.city,
+            country:geo.country,
+            region: geo.region,
+            ll:geo.ll
+          } 
+        });
+      client.close();
+    });
+  } catch (e) {
+    console.log("eerr", e);
+  }
+};
+
+const nodeMailer = function (email) {
   var transporter = nodemailer.createTransport(
     "smtps://sfly.suppor%40gmail.com:sfly123741@smtp.gmail.com"
   );
   // setup e-mail data with unicode symbols
   var mailOptions = {
     from: '"Sfly Support " <sfly.suppor@gmail.com>', // sender address
-    to: "k_ouazari@hotmail.fr", // list of receivers
+    //to: email, // list of receivers
+    to: email, // list of receivers
     subject: "Newsletters ✔", // Subject line
     text: "Merci pour votre subscription à notre newsletters", // plaintext body
     html: "<b>Merci pour votre subscription à notre newsletters ????</b>" // html body
@@ -40,11 +82,19 @@ app.get("/send-email", function(req, res) {
     if (error) {
       return console.log(error);
     }
-
-    console.log("Message sent: " + info.response);
+    //console.log("Message sent: " + info.response);
     transporter.close();
-    return res.send(info);
+    res.status(200).send(info.response);
   });
+}
+
+app.post("/send-email", function(req, res) {
+  let email = req.query.email;
+
+    insertEmail(email,req.ip);//Insert Into Db
+    nodeMailer(email)// Send Email
+
+ 
 });
 
 app.listen(port);
--- src/controllers/ApiRequest.js
+++ src/controllers/ApiRequest.js
@@ -1,41 +1,35 @@
 import {Request} from "./Request";
-import { config } from "./config";
+
+//export const API_URL="https://sfly.herokuapp.com";
+export const API_URL="http://localhost:8080";
 
 export const ApiRequest = class ApiRequest {
-  constructor(){}
 
   static async post(route, params){
-    console.log('route',route)
-    console.log('params',params)
-    
-    console.log('pro',process)
-
-    return await Request.fetch(config.apiUrl + route + params, {
+    return await Request.fetch(API_URL + '/' + route + '?email=' + params, {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-      }
-    }).catch(err=>console.log('ERROR POST',err));
+      },
+    });
   }
   static async put(route, params){
-    return await Request.fetch(process.env.API_URL  + route, {
+    return await Request.fetch(API_URL + '/' + route, {
       method: 'PUT',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        
       },
       body: JSON.stringify(params)
     });
   }
   static async get(route) {
-    return await Request.fetch(process.env.API_URL + route, {
+    return await Request.fetch(API_URL  + route, {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-       
       },
     });
   }
